67b7c7a7754760665427bdaffe95dd48cd584730
tac0thepac0 <paco.rodriguez.vara@gmail.com>
2025-02-01 01:01:34 -0600

    docs(src/main.py): Created module and function docstrings and TODO comments

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+""" CyberPatriot unofficial score display system """
+
 from requests import get
 from flask import Flask, render_template
 
@@ -10,6 +12,8 @@ data = {
         }
 
 def get_scores():
+    """ Requests teams outlined in data and populates dictionary """
+    # TODO: Rethink data handling logic
     for team in data:
         req = get(
             url="https://scoreboard.uscyberpatriot.org/api/team/scores.php",
@@ -19,6 +23,8 @@ def get_scores():
         data[team] = req.json()['data'][0]
 
 def get_team_scores(team):
+    """ Returns data for param team from dictionary data """
+    # TODO: Re-evaluate logic for handling team scores
     if team in data:
         if not data[team]:
             get_scores()
@@ -26,17 +32,21 @@ def get_team_scores(team):
         return data[team]
     else:
         return {}
-    
+
 def team_not_found(team):
+    """ Returns content for case where a team is not found """
+    # TODO: Create html file
     return f"Team {team} not found", 404
 
 @app.route("/")
 def index():
+    """ Populate/Update dicitonary data and serve template with data """
     get_scores()
     return render_template('index.html', out=data)
 
 @app.route("/<team>")
 def team_scores(team=None):
+    """ Serve template team.html with team specified in the URL """
     if team in data:
         return render_template('team.html', out=get_team_scores(team))
     else:
